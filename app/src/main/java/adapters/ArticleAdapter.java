bd089068d9568cd62824925ee828bfac07a79b26
WangJunxin <wangjunxit12@163.com>
2016-10-10 11:38:34 +0800

    第四次提交

--- app/src/main/java/adapters/ArticleAdapter.java
+++ app/src/main/java/adapters/ArticleAdapter.java
@@ -8,60 +8,28 @@ import android.widget.TextView;
 
 import com.example.devtf.devtf.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import bean.Article;
-import listener.OnItemClickListener;
 
 /**
  * Created by Administrator on 2016/9/20.
  */
 
-public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder> {
-private List<Article> mDataSet=new ArrayList<>();
-    private OnItemClickListener<Article> itemClickListener;
+public class ArticleAdapter extends RecyclerBaeAdapter<Article , ArticleAdapter.ArticleViewHolder> {
 
-    public ArticleAdapter(List<Article> mDataSet) {
-        this.mDataSet = mDataSet;
+
+    @Override
+    protected void bindDataToItemView(ArticleViewHolder viewholder, Article item) {
+        viewholder.title.setText(item.title);
+        viewholder.author.setText(item.author);
+        viewholder.time.setText(item.publishTime);
     }
 
     @Override
-    public ArticleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ArticleViewHolder( LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.article_item,parent,false
-        ));
+    public ArticleViewHolder onCreateViewHolder(ViewGroup parent, int layoutId) {
+        return new ArticleViewHolder(LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.article_item,parent,false));
     }
 
-    @Override
-    public void onBindViewHolder(ArticleViewHolder holder, int position) {
-         final   Article item=getItem(position);
-        holder.title.setText(item.title);
-        holder.author.setText(item.author);
-        holder.time.setText(item.publishTime);
-        setupItemViewClickListener(holder,item);
-    }
-
-    @Override
-    public int getItemCount() {
-        return mDataSet.size();
-    }
-    protected Article getItem(int postion){
-        return mDataSet.get(postion);
-    }
-    public void setOnItemClickListener(OnItemClickListener<Article> itemClickListener){
-        this.itemClickListener=itemClickListener;
-    }
-    protected void setupItemViewClickListener(ArticleViewHolder holder,
-                                              final Article item){
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                itemClickListener.OnClick(item);
-            }
-        });
-
-    }
     static class ArticleViewHolder extends RecyclerView.ViewHolder{
         public TextView title;
         public TextView author;
--- app/src/main/java/adapters/Menuadapter.java
+++ app/src/main/java/adapters/Menuadapter.java
@@ -1,8 +1,6 @@
 package adapters;
 
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.Adapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,61 +9,27 @@ import android.widget.TextView;
 
 import com.example.devtf.devtf.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import bean.MenuItem;
-import listener.OnItemClickListener;
 
 /**
  * Created by Administrator on 2016/9/14.
  */
-public  class Menuadapter extends Adapter<Menuadapter.MenuViewHolder> {
-    List<MenuItem> mDataSet=new ArrayList<>();
-    OnItemClickListener<MenuItem> mItemClickListener;
-    private static final  String TAG="Menuadapter";
-    public Menuadapter(List<MenuItem> mDataSet) {
-        this.mDataSet = mDataSet;
-        Log.i(TAG, "Menuadapter: "+mDataSet.size());
+public  class Menuadapter extends RecyclerBaeAdapter<MenuItem, Menuadapter.MenuViewHolder> {
+
+    @Override
+    public MenuViewHolder onCreateViewHolder(ViewGroup parent, int layoutId) {
+        return new MenuViewHolder(LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.menu_item,parent,false))
+                ;
     }
 
     @Override
-    public MenuViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new MenuViewHolder(LayoutInflater.
-                from(parent.getContext()).inflate(R.layout.menu_item,parent,false));
-    }
-
-    @Override
-    public void onBindViewHolder(MenuViewHolder holder, int position) {
-        final MenuItem item=mDataSet.get(position);
-        Log.i(TAG, "Menuadapter: "+item.getName());
-        holder.nameTextView.setText(item.getName());
-        holder.userImageView.setImageResource(item.getResId());
-        setupItemViewClickListener(holder,item);
+    protected void bindDataToItemView(MenuViewHolder viewholder, MenuItem item) {
+        viewholder.nameTextView.setText(item.getName());
+        viewholder.userImageView.setImageResource(item.getResId());
     }
 
 
-    @Override
-    public int getItemCount() {
-        return mDataSet.size();
-    }
-
-   public void setOnItemClickListener(OnItemClickListener<MenuItem> mItemClickListener){
-       this.mItemClickListener=mItemClickListener;
-   }
-   protected void setupItemViewClickListener(MenuViewHolder viewHolder,final MenuItem itme){
-       viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if(mItemClickListener!=null){
-                   mItemClickListener.OnClick(itme);
-               }
-           }
-       });
-   }
-   protected  MenuItem getItem(int postion){
-       return mDataSet.get(postion);
-   }
      static class MenuViewHolder extends RecyclerView.ViewHolder{
         public ImageView userImageView;
         public TextView nameTextView;
